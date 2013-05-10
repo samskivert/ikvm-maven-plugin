7b811bc462483c381fefb86601ac94ec4d98fbfe
Jamie Doornbos <jamie@threerings.net>
2013-05-09 22:24:56 -0700

    These aren't needed anymore

--- src/main/java/com/samskivert/IkvmMavenMojo.java
+++ src/main/java/com/samskivert/IkvmMavenMojo.java
@@ -21,10 +21,6 @@ import org.codehaus.plexus.util.cli.CommandLineUtils;
 import org.codehaus.plexus.util.cli.Commandline;
 
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
@@ -115,12 +111,6 @@ public class IkvmMavenMojo extends AbstractMojo
      * @parameter expression="${ikvm.treatWarningsAsErrors}" default-value="false"
      */
     public boolean treatWarningsAsErrors;
-
-    /**
-     * The local repository to use when resolving dependencies.
-     * @parameter default-value="${localRepository}"
-     */
-    public ArtifactRepository localRepository;
 
     public void execute () throws MojoExecutionException {
         // create our target directory if needed (normally the jar plugin does this, but we don't
@@ -348,13 +338,4 @@ public class IkvmMavenMojo extends AbstractMojo
 
     /** @parameter default-value="${project}" */
     private MavenProject _project;
-
-    /** @component */
-    private ArtifactResolver _resolver;
-
-    /** @component */
-    private ArtifactMetadataSource _metadataSource;
-
-    /** @component */
-    private ArtifactFactory _factory;
 }
